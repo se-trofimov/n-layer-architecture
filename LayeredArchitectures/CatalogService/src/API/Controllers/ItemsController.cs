48900a21e98c74ab4985144e8cfa80613ca9785a
Serhii Trofimov <trofimov.developer@gmail.com>
2022-10-30 18:40:01 +0100

    HATEOAS for Items

--- LayeredArchitectures/CatalogService/src/API/Controllers/ItemsController.cs
+++ LayeredArchitectures/CatalogService/src/API/Controllers/ItemsController.cs
@@ -3,8 +3,10 @@ using CatalogService.Application.Common;
 using CatalogService.Application.Dtos;
 using CatalogService.Application.UseCases.Items.Commands;
 using CatalogService.Application.UseCases.Items.Queries;
+using CatalogService.Domain.Entities;
 using MediatR;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.Net.Http.Headers;
 
 namespace API.Controllers;
 
@@ -12,13 +14,15 @@ namespace API.Controllers;
 public class ItemsController : ControllerBase
 {
     private readonly IMediator _mediator;
+    private readonly LinkGenerator _linkGenerator;
 
-    public ItemsController(IMediator mediator)
+    public ItemsController(IMediator mediator, LinkGenerator linkGenerator)
     {
         _mediator = mediator ?? throw new ArgumentNullException(nameof(mediator));
+        _linkGenerator = linkGenerator ?? throw new ArgumentNullException(nameof(linkGenerator));
     }
 
-    [HttpGet]
+    [HttpGet(Name = nameof(GetItems))]
     public async Task<ActionResult<PagedList<ItemDto>>> GetItems(int categoryId, QueryParams queryParams)
     {
         var result = await _mediator.Send(new GetItemsQuery(categoryId, queryParams.PageNumber, queryParams.PageSize));
@@ -31,37 +35,105 @@ public class ItemsController : ControllerBase
             result.HasNext,
             result.HasPrevious
         };
-        Response.Headers.Add("X-Pagination", JsonSerializer.Serialize(metadata)); 
-        return Ok(result);
+        Response.Headers.Add("X-Pagination", JsonSerializer.Serialize(metadata));
+        var mediaType = HttpContext.Request.Headers["Accept"].FirstOrDefault();
+        MediaTypeHeaderValue.TryParse(mediaType, out MediaTypeHeaderValue? outMediaType);
+
+        if (!outMediaType!.SubTypeWithoutSuffix.EndsWith("hateoas", StringComparison.InvariantCultureIgnoreCase))
+        {
+            return Ok(result);
+        }
+
+        var listOfWrappedItems = new List<LinkWrapper<ItemDto>>(result.Count);
+        foreach (var item in result)
+        {
+            var categoryLinks = CreateLinksForItem(categoryId, item.Id);
+            listOfWrappedItems.Add(new LinkWrapper<ItemDto>(item, categoryLinks));
+        }
+
+        var categoriesLinks = GenerateItemsLinks(categoryId);
+        var response = new LinkWrapper<List<LinkWrapper<ItemDto>>>(listOfWrappedItems, categoriesLinks);
+        return Ok(response);
     }
 
     [HttpGet("{id:int}", Name = nameof(GetItemById))]
     public async Task<ActionResult<ItemDto>> GetItemById(int categoryId, int id)
     {
-        var result = await _mediator.Send(new GetItemByIdQuery(id, categoryId));
-        return Ok(result);
+        var item = await _mediator.Send(new GetItemByIdQuery(id, categoryId));
+        var mediaType = HttpContext.Request.Headers["Accept"].FirstOrDefault();
+        MediaTypeHeaderValue.TryParse(mediaType, out MediaTypeHeaderValue? outMediaType);
+
+        if (!outMediaType!.SubTypeWithoutSuffix.EndsWith("hateoas", StringComparison.InvariantCultureIgnoreCase))
+        {
+            return Ok(item);
+        }
+
+        var itemLinks = CreateLinksForItem(categoryId, item.Id);
+        var response = new LinkWrapper<ItemDto>(item, itemLinks);
+
+        return Ok(response);
     }
 
 
-    [HttpPost]
+    [HttpPost(Name = nameof(CreateItem))]
     public async Task<ActionResult<ItemDto>> CreateItem(int categoryId, [FromBody] CreateItemCommand command)
     {
         command.CategoryId = categoryId;
-        var result = await _mediator.Send(command);
-        return CreatedAtRoute(nameof(GetItemById), new { categoryId, result.Id }, result);
+        var item = await _mediator.Send(command);
+
+        var mediaType = HttpContext.Request.Headers["Accept"].FirstOrDefault();
+        MediaTypeHeaderValue.TryParse(mediaType, out MediaTypeHeaderValue? outMediaType);
+
+        if (!outMediaType!.SubTypeWithoutSuffix.EndsWith("hateoas", StringComparison.InvariantCultureIgnoreCase))
+        {
+            return CreatedAtRoute(nameof(GetItemById), new { categoryId, item.Id }, item);
+        }
+
+        var itemLinks = CreateLinksForItem(categoryId, item.Id);
+        var response = new LinkWrapper<ItemDto>(item, itemLinks);
+        return CreatedAtRoute(nameof(GetItemById), new { categoryId, id = item.Id }, response);
     }
 
-    [HttpPut]
-    public async Task<ActionResult> ChangeItem(int categoryId, [FromBody] ChangeItemCommand command)
+    [HttpPut("{id:int}", Name = nameof(ChangeItem))]
+    public async Task<ActionResult> ChangeItem(int categoryId, int itemId, [FromBody] ChangeItemCommand command)
     {
+        command.Id = itemId;
         var result = await _mediator.Send(command);
         return NoContent();
     }
 
-    [HttpDelete("{id:int}")]
+    [HttpDelete("{id:int}", Name = nameof(DeleteItem))]
     public async Task<ActionResult> DeleteItem(int categoryId, int id)
     {
         var result = await _mediator.Send(new DeleteItemCommand(id, categoryId));
         return NoContent();
+    }
+
+    private List<Link> GenerateItemsLinks(int categoryId)
+    {
+        var links = new List<Link>
+        {
+            new Link(_linkGenerator.GetUriByAction(HttpContext,  nameof(GetItems), values: new {categoryId }),
+                "self",
+                "GET"),
+        };
+        return links;
+    }
+
+    private List<Link> CreateLinksForItem(int categoryId, int id)
+    {
+        var links = new List<Link>
+        {
+            new Link(_linkGenerator.GetUriByAction(HttpContext, nameof(GetItemById), values: new {categoryId, id }),
+            "self",
+                "GET"),
+            new Link(_linkGenerator.GetUriByAction(HttpContext, nameof(DeleteItem), values: new {categoryId, id }),
+                "delete_item",
+                "DELETE"),
+            new Link(_linkGenerator.GetUriByAction(HttpContext, nameof(ChangeItem), values: new {categoryId, id }),
+                "update_item",
+                "PUT")
+        };
+        return links;
     }
 }
